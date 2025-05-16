74a6d6694ca2925664f090061e26db8b569793f5
SreekuttanCS <sreekuttancs26@gmail.com>
2025-05-16 20:28:58 +0530

    created an add sub category where we can add sub categories

--- frontend/src/components/product Setup/ProductSetup.jsx
+++ frontend/src/components/product Setup/ProductSetup.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AddProduct from "./AddProduct";
 import AddCategory from "./AddCategory";
+import AddSubCategory from "./AddCategory";
 
 const ProductSetup = () => {
   return (
     <div className="absolute z-10 top-2/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-white shadow rounded-2xl border-1 border-gray-400 w-1/2  h-3/4">
       {/* <AddProduct /> */}
-      <AddCategory />
+      {/* <AddCategory /> */}
+      <AddSubCategory />
     </div>
   );
 };
